ca62588ef0049d42744d5324f504db1aed625576
Robin Chowdhury <robinch88@gmail.com>
2014-03-04 16:38:43 +0100

    Added support for changing languages

---
+++ js/jsonLoader.js
@@ -0,0 +1,24 @@
+// $.getJSON("strings.json", function(json) {
+//     console.log(json); // this will show the info it in firebug console
+// });
+
+var json = {
+    strings:{
+        navigation:{
+            start:["start", "start"],
+            services:["tjänster", "services"],
+            projects:["tidigare projekt", "earlier projects"],
+            about:["om oss", "about"],
+            contact:["kontakt", "contact"]
+        }
+        
+    }
+};
+
+// for(var hej in json.strings.navigation) {
+//     alert(json.strings.navigation[hej][0]);
+// }
+
+// for(var hej in json.strings.navigation) {
+//     alert(json.strings.navigation[hej][1]);
+// }
