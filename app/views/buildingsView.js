57f6f36292ccb6b022ac9d04fa104486df00ffcc
MartinAndersen <martin.erik.andersen91@gmail.com>
2014-03-17 17:43:01 +0100

    Started working on the design of the building-images

--- app/views/buildingsView.js
+++ app/views/buildingsView.js
@@ -3,12 +3,14 @@ function buildingsView(jsonObject) {
         var buildingsDiv = document.getElementById("buildingsPart");
 
         var table = document.createElement("table");
+        table.setAttribute('Class', "buildingsPartey")
         var td = document.createElement("td");
         var tr = document.createElement("tr");
 
         //Add first image of building
         var img = document.createElement('img');
         img.setAttribute('src', jsonObject.imgrefr);
+        img.setAttribute('Class', "buildingPic");
         //img.onclick = func;
         td.appendChild(img);
         tr.appendChild(td);
