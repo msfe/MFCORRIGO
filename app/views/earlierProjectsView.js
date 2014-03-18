6151bbff131db58049f6941e34a560c7922619d3
MartinAndersen <martin.erik.andersen91@gmail.com>
2014-03-18 12:33:16 +0100

    Added grid systems/3 column-system for buildnings

--- app/views/earlierProjectsView.js
+++ app/views/earlierProjectsView.js
@@ -23,8 +23,80 @@ var earlierProjectsView = function() { //listOfJsonObjects) {
         imgrefr5:[null],
         about:["Tihi, svensk flagga"]    
     };
-    var jsonList = [this.json, this.json2];
+    this.json3 = {
+        id:["1"],
+        name:["betonghus"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/hus1.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Kalle kalle"]
+    };
+    this.json4 = {
+        id:["4"],
+        name:["betonghus2"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/swedishflag.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Tihi, svensk flagga"]    
+    };
+    this.json5 = {
+        id:["1"],
+        name:["betonghus"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/hus1.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Kalle kalle"]
+    };
+    this.json6 = {
+        id:["4"],
+        name:["betonghus2"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/swedishflag.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Tihi, svensk flagga"]    
+    };
+    this.json7 = {
+        id:["1"],
+        name:["betonghus"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/hus1.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Kalle kalle"]
+    };
+    this.json8 = {
+        id:["4"],
+        name:["betonghus2"],
+        type:["byggnader"],
+        year:[null],
+        imgrefr:["res/swedishflag.jpg"],
+        imgrefr2:[null],
+        imgrefr3:[null],
+        imgrefr4:[null],
+        imgrefr5:[null],
+        about:["Tihi, svensk flagga"]    
+    };
+    var jsonList = [this.json, this.json2, this.json3, this.json4, this.json5, this.json6, this.json7, this.json8];
     for(var jsonObject in jsonList) {
-        new buildingsView(jsonList[jsonObject]);
+        new buildingsView(jsonList[jsonObject], jsonObject%3);
     }
 }
