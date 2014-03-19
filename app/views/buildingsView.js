fbb197075b8a066c41e88fed6de18f3ec17db8b9
MartinAndersen <martin.erik.andersen91@gmail.com>
2014-03-19 13:40:15 +0100

    started working on making the buildnings clickable

--- app/views/buildingsView.js
+++ app/views/buildingsView.js
@@ -1,38 +1,46 @@
 //Creates an element with a dishImage and text
 function buildingsView(project, columnNumber) {
-        var table = document.createElement("table");
-        table.setAttribute('Class', "buildingsPartey")
-        var td = document.createElement("td");
-        var tr = document.createElement("tr");
+    var table = document.createElement("table");
+    table.setAttribute('Class', "buildingsPartey");
+    var td = document.createElement("td");
+    var tr = document.createElement("tr");
 
-        //Add first image of building
-        var div = document.createElement("div");
-        div.setAttribute('Class', "imageWrapper");
-        var img = document.createElement('img');
-        img.setAttribute('src', 'res/projekt/' + project.imgrefr);
-        img.setAttribute('Class', "buildingPic");
-        //img.onclick = func;
-        div.appendChild(img);
-        td.appendChild(div);
-        tr.appendChild(td);
-        table.appendChild(tr);
+    //Add first image of building
+    var div = document.createElement("div");
+    div.setAttribute('Class', "imageWrapper");
+    var img = document.createElement('img');
+    img.setAttribute('src', 'res/projekt/' + project.imgrefr);
+    img.setAttribute('Class', "buildingPic");
 
-        //Add name of building
-        var td = document.createElement("td");
-        var tr = document.createElement("tr");
-        td.appendChild(document.createTextNode(project.name));
-        tr.appendChild(td);
-        table.appendChild(tr);
+    //img.onclick = func;
+    div.appendChild(img);
+    td.appendChild(div);
+    tr.appendChild(td);
+    table.appendChild(tr);
+
+    //Add name of building
+    var td = document.createElement("td");
+    var tr = document.createElement("tr");
+    td.appendChild(document.createTextNode(project.name));
+    tr.appendChild(td);
+    table.appendChild(tr);
+    table.setAttribute('onclick', "function() { createPopUp('project'); }");
+    //table.setAttribute('onclick', "createPopUp(project)");
 
 
-        if(columnNumber == 0) {
-            var buildingsDiv = document.getElementById("columnnr1");
-        }
-        else if(columnNumber == 1) {
-            var buildingsDiv = document.getElementById("columnnr2");
-        }
-        else {//(columnNumber == 2) {
-            var buildingsDiv = document.getElementById("columnnr3");
-        }
-        buildingsDiv.appendChild(table);
+    if(columnNumber == 0) {
+        var buildingsDiv = document.getElementById("columnnr1");
     }
+    else if(columnNumber == 1) {
+        var buildingsDiv = document.getElementById("columnnr2");
+    }
+    else {//(columnNumber == 2) {
+        var buildingsDiv = document.getElementById("columnnr3");
+    }
+    
+    buildingsDiv.appendChild(table);
+}
+
+function createPopUp(project) {
+    alert(project);
+}
