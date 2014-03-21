aae3774de32c451273bd4e93991803e60fab8e08
Robin Chowdhury <robinch88@gmail.com>
2014-03-21 12:37:06 +0100

    Refractored more. MVC, yo

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -32,11 +32,8 @@ angular.module('mfcorrigoApp.controllers', ['mfcorrigoApp.factories'])
     text: 'Fler än 30 stycken tillbyggnader'
   }];
 })
+
 .controller('FilterController', function($scope, $rootScope, $modal, $log, Model) {
-  
-  $scope.getFavorites = function(){
-    return Model.getAll();
-  };
 
   $scope.favorites = Model.getAll();
 
@@ -69,6 +66,7 @@ angular.module('mfcorrigoApp.controllers', ['mfcorrigoApp.factories'])
     $rootScope.type = type;
   };
 })
+
 .controller('ProjectViewController', function($scope, $modalInstance, $rootScope, project, Model) {
   $scope.project = project;
   $scope.myInterval = 5000;
@@ -131,6 +129,7 @@ angular.module('mfcorrigoApp.controllers', ['mfcorrigoApp.factories'])
 
 })
 .controller('LoadModel', function ($scope, $http, $rootScope) {
+  var fakeDb = new FakeDb();
 
     $scope.url = 'php/getDbData.php'; // The url of the php-request
     $http.post($scope.url, {}) // Create the http post request
@@ -139,13 +138,13 @@ angular.module('mfcorrigoApp.controllers', ['mfcorrigoApp.factories'])
       $rootScope.data = data;
       if($scope.data.indexOf("mysqli_connect()") != -1){
         console.log("Failed to connect to DB, faking model");
-        $rootScope.data = fakeModel();
+        $rootScope.data = fakeDb.getDb();
       }
       makeImageList();
     })
     .error(function(data, status) {
      console.log("No PHP-service found, faking model");
-     $rootScope.data = fakeModel();
+     $rootScope.data = fakeDb.getDb();
      $scope.status = status;         
      makeImageList();
    });
@@ -172,109 +171,5 @@ angular.module('mfcorrigoApp.controllers', ['mfcorrigoApp.factories'])
         $rootScope.data[project].images = images;    
       }
     }
-    
-
-    function fakeModel() {
-      var data = [
-      {
-        "id": "1",
-        "name": "byggnader1",
-        "type": "buildings",
-        "year": null,
-        "imgrefr": "betonghus1.png",
-        "imgrefr2": "betonghus2.png",
-        "imgrefr3": null,
-        "imgrefr4": null,
-        "imgrefr5": null,
-        "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-      },
-      {
-       "id": "2",
-       "name": "bathroom1",
-       "type": "bathroom",
-       "year": null,
-       "imgrefr": "exbild1.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-     {
-       "id": "3",
-       "name": "interiours1",
-       "type": "interiours",
-       "year": null,
-       "imgrefr": "exbild2.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-     {
-       "id": "4",
-       "name": "byggnader2",
-       "type": "buildings",
-       "year": null,
-       "imgrefr": "exbild3.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-     {
-       "id": "5",
-       "name": "fromConstruction1",
-       "type": "fromConstruction",
-       "year": null,
-       "imgrefr": "exbild4.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-     {
-       "id": "6",
-       "name": "bathrooms2",
-       "type": "bathroom",
-       "year": null,
-       "imgrefr": "exbild5.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-     {
-       "id": "6",
-       "name": "bathrooms3",
-       "type": "bathroom",
-       "year": null,
-       "imgrefr": "exbild6.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
-     },
-
-     {
-       "id": "7",
-       "name": "reconstruction1",
-       "type": "reconstruction",
-       "year": null,
-       "imgrefr": "exbild7.jpg",
-       "imgrefr2": "betonghus2.png",
-       "imgrefr3": null,
-       "imgrefr4": null,
-       "imgrefr5": null,
-       "about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng."
-     }
-     ];
-     return data;
-   }
  });
 
---
+++ app/js/fakeDb.js
@@ -0,0 +1,107 @@
+function FakeDb(){
+
+	this.db = [
+	{
+		"id": "1",
+		"name": "byggnader1",
+		"type": "buildings",
+		"year": null,
+		"imgrefr": "betonghus1.png",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "2",
+		"name": "bathroom1",
+		"type": "bathroom",
+		"year": null,
+		"imgrefr": "exbild1.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "3",
+		"name": "interiours1",
+		"type": "interiours",
+		"year": null,
+		"imgrefr": "exbild2.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "4",
+		"name": "byggnader2",
+		"type": "buildings",
+		"year": null,
+		"imgrefr": "exbild3.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "5",
+		"name": "fromConstruction1",
+		"type": "fromConstruction",
+		"year": null,
+		"imgrefr": "exbild4.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "6",
+		"name": "bathrooms2",
+		"type": "bathroom",
+		"year": null,
+		"imgrefr": "exbild5.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+	{
+		"id": "6",
+		"name": "bathrooms3",
+		"type": "bathroom",
+		"year": null,
+		"imgrefr": "exbild6.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng"
+	},
+
+	{
+		"id": "7",
+		"name": "reconstruction1",
+		"type": "reconstruction",
+		"year": null,
+		"imgrefr": "exbild7.jpg",
+		"imgrefr2": "betonghus2.png",
+		"imgrefr3": null,
+		"imgrefr4": null,
+		"imgrefr5": null,
+		"about": "Ett betonghus byggt med prefabricerade sandwich betongelement, sidobyggnad klädd med cortenplåt. Villan var med i TV8 programmet \"Hustoppen\" och fick 23 poäng."
+	}
+	];
+
+	this.getDb = function(){
+		return this.db;
+	}
+
+};
